840e53950bc3114256a70450a5b1f266ae502831
Gustavo L. M. de Oliveira <gustavo.l.m.de.oliveira@scania.com>
2024-04-26 13:35:18 +0200

    Better search instructions

--- src/components/stores/myai-search-store/search-store.ts
+++ src/components/stores/myai-search-store/search-store.ts
@@ -23,17 +23,17 @@ export const { state: searchState } = createStore<searchStore>({
   messages: [
     {
       role: Role.SYSTEM,
-      content:
-        "Your task is to generate a Google Shopping search that best matches the user's need. Your output will be used to search for products on Google Shopping. You are allowed to search for products by name or brand if you see fit. IF YOU DO NOT UNDERSTAND THE USER'S REQUEST, JUST RETURN WHAT THEY WROTE.",
-    },
-    {
-      role: Role.SYSTEM,
-      content:
-        'Feel free to utilize any relevant information provided by the user to refine your search, such as product category, price range, or any specific preferences mentioned.',
-    },
-    {
-      role: Role.SYSTEM,
-      content: 'YOU MAY ONLY ANSWER WITH THE SEARCH TEXT. NOTHING ELSE.',
+      content: `
+      You will be provided with a user request. YOUR TASK is to generate a Google Shopping search text.
+      
+      Here are your instructions:
+
+      1.CREATE A GOOGLE SHOPPING SEARCH TERM THAT WILL HELP THE USER FIND WHAT PRODUCT THEY ARE LOOKING FOR.
+
+      2.YOU MUST ANSWER ONLY WITH THE SHOPPING SEARCH TERM, NOTHING ELSE.
+      
+      3.IF YOU DO NOT UNDERSTAND THE USER'S REQUEST, STILL CREATE A SEARCH TEXT.
+      `
     },
   ],
 
