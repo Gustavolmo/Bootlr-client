65dda33c9bc99917bd3372cb3c74686b03cafa53
Gustavo L. M. de Oliveira <gustavo.l.m.de.oliveira@scania.com>
2024-05-08 20:04:29 +0200

    hotfix: populate products in focus error

--- src/components/stores/myai-chat-store/chat-helper.ts
+++ src/components/stores/myai-chat-store/chat-helper.ts
@@ -104,10 +104,12 @@ const populateProductsInFocus = (productReference: string[]) => {
   if (!productReference) return;
 
   productState.productsInFocus.length = 0;
-  
+
   const matchedProducts = productState.shoppingResults.filter(product => {
     return productReference.some(reference => product.title.includes(reference));
   });
+
+  if(!matchedProducts) return;
 
   const uniqueProducts = matchedProducts.filter((match, index, self) => {
     return self.findIndex(obj => obj.position === match.position) === index;
