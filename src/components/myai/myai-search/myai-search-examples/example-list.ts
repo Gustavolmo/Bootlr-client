10f42b1c05eb74ef93910348670f2c9113fd07e8
Gustavo L. M. de Oliveira <gustavo.l.m.de.oliveira@scania.com>
2024-05-09 10:34:38 +0200

    suggestion randomizer

---
+++ src/components/myai/myai-search/myai-search-examples/example-list.ts
@@ -0,0 +1,27 @@
+export const ideasForExamples = [
+  'mid-century home decoration',
+  'my wine collection display',
+  'beach vacation activities',
+  'my home office setup',
+];
+
+export const showMeExamples = [
+  'beautiful things',
+  'funny gifts',
+  'halloween masks',
+  'smart home devices'
+];
+
+export const canYouFindExamples = [
+  'kitchen gadgets',
+  'fitness equipment',
+  'pet supplies',
+  'tech accessories',
+];
+
+export const iAmLookingForExamples = [
+  'gifts for my kids',
+  'home improvement tools',
+  'outdoor gear',
+  'travel accessories'
+];
---
+++ src/components/myai/myai-search/myai-search-examples/myai-search-examples.tsx
@@ -0,0 +1,67 @@
+import { Component, State, h } from '@stencil/core';
+import { searchState } from '../../../stores/myai-search-store/search-store';
+import {
+  canYouFindExamples,
+  iAmLookingForExamples,
+  ideasForExamples,
+  showMeExamples,
+} from './example-list';
+
+@Component({
+  tag: 'myai-search-examples',
+  styleUrl: './myai-search-examples.css',
+  shadow: true,
+})
+export class MyaiSearchExamples {
+  @State() ideasFor: string;
+  @State() showMe: string;
+  @State() canYouFind: string;
+  @State() iAmLookingFor: string;
+
+  componentWillRender() {
+    this.ideasFor = this.suggestionRandomizer(ideasForExamples);
+    this.showMe = this.suggestionRandomizer(showMeExamples);
+    this.canYouFind = this.suggestionRandomizer(canYouFindExamples);
+    this.iAmLookingFor = this.suggestionRandomizer(iAmLookingForExamples);
+  }
+
+  private suggestionRandomizer = (examples: string[]): string => {
+    const randomIndex = Math.floor(Math.random() * examples.length);
+    console.log(examples[0]);
+    return examples[randomIndex];
+  };
+
+  render() {
+    return (
+      <article class="search-example-wrap">
+        <div
+          class="search-example"
+          onClick={() => searchState.processSearchRequest(`Ideas for ${this.ideasFor}`)}
+        >
+          Ideas for <span class="gray">{this.ideasFor}</span>
+        </div>
+
+        <div
+          class="search-example"
+          onClick={() => searchState.processSearchRequest(`Show me ${this.showMe}`)}
+        >
+          Show me <span class="gray">{this.showMe}</span>
+        </div>
+
+        <div
+          class="search-example"
+          onClick={() => searchState.processSearchRequest(`Can you find ${this.canYouFind}`)}
+        >
+          Can you find <span class="gray">{this.canYouFind}</span>
+        </div>
+
+        <div
+          class="search-example"
+          onClick={() => searchState.processSearchRequest(`I am looking for ${this.iAmLookingFor}`)}
+        >
+          I am looking for <span class="gray">{this.iAmLookingFor}</span>
+        </div>
+      </article>
+    );
+  }
+}
