e42520c225778b99944f43bb0b2f1ae15cb9c045
Gustavo L. M. de Oliveira <gustavo.l.m.de.oliveira@scania.com>
2024-05-09 18:38:35 +0200

    todays deals section

--- src/http-definitions/endpoints.ts
+++ src/http-definitions/endpoints.ts
@@ -2,9 +2,11 @@ export const apiUrl = {
   local: {
     bootlrSearch: 'http://localhost:4000/bootlr-search',
     bootlrChat: 'http://localhost:4000/bootlr-chat',
+    bootlrOffers: 'http://localhost:4000/bootlr-offers'
   },
   prod: {
     bootlrSearch: 'https://staging-bootlr-99p2.encr.app/bootlr-search',
     bootlrChat: 'https://staging-bootlr-99p2.encr.app/bootlr-chat',
+    bootlrOffers: 'https://staging-bootlr-99p2.encr.app/bootlr-offers'
   },
 };
