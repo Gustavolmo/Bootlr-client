285dc5ddebf0fab76409b0385ceb38a21f5fb933
Gustavo L. M. de Oliveira <gustavo.l.m.de.oliveira@scania.com>
2024-05-13 13:23:56 +0200

    keywords adjustment

--- src/components/stores/myai-search-store/search-store.ts
+++ src/components/stores/myai-search-store/search-store.ts
@@ -27,13 +27,13 @@ export const searchStore = createStore<searchStore>({
       role: Role.SYSTEM,
       content: `
       You are an expert in generating search keywords for product searches in Amazon and Google shopping,
-      you are tasked with transforming the user request into 3 accurate keywords to best represent the user intention.
+      you are tasked with transforming the user request into 2 accurate keywords to best represent the user intention.
 
       Here are your instructions:
 
       1. THE SEARCHES ARE MADE IN SWEDEN, USE SWEDISH KEYWORDS.
 
-      2. GENERATE UP TO 3 KEYWORDS FOLLOWING THIS FORMAT: "keyword1 keyword2 keyword3"
+      2. GENERATE UP TO 2 KEYWORDS FOLLOWING THIS FORMAT: "keyword1 keyword2"
       
       3. UNDERSTAND WHAT THE USER WANTS TO BUY AND GENERATE ACCURATE SEARCH KEYWORDS.
 
